d3985bbe4e0ca148b1cf52ec814278bdb4489216
Wayne <wayned27@outlook.com>
2025-03-21 12:14:02 -0400

    Added phpdoc on middleware

--- app/src/Exceptions/HttpNotAcceptableException.php
+++ app/src/Exceptions/HttpNotAcceptableException.php
@@ -1,13 +1,39 @@
 <?php
 
+/**
+ * Namespace declaration for the App\Exceptions namespace.
+ */
 namespace App\Exceptions;
 
 use Slim\Exception\HttpSpecializedException;
 
+/**
+ * Class HttpNotAcceptableException
+ *
+ * Exception thrown when the request is not acceptable (HTTP status code 406).
+ * This occurs when the client requests a content type other than JSON.
+ *
+ * @package App\Exceptions
+ */
 class HttpNotAcceptableException extends HttpSpecializedException
 {
+    /**
+     * @var int The HTTP status code for "Not Acceptable".
+     */
     protected $code = 406;
+
+    /**
+     * @var string Default error message for "Not Acceptable".
+     */
     protected $message = 'Not Acceptable';
+
+    /**
+     * @var string Title for the exception, shown in error details.
+     */
     protected string $title = '406 Not Acceptable';
+
+    /**
+     * @var string Detailed description for the exception.
+     */
     protected string $description = 'Request is Not Acceptable, Values other than /json is not Acceptable!';
 }
--- app/src/Middleware/ContentNegotiationMiddleware.php
+++ app/src/Middleware/ContentNegotiationMiddleware.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Middleware for Content Negotiation.
+ *
+ * Ensures that the incoming request has an acceptable content type,
+ * specifically 'application/json'. Throws an exception if the content type is unacceptable.
+ *
+ * @package App\Middleware
+ */
+
 declare(strict_types=1);
 
 namespace App\Middleware;
@@ -11,22 +20,53 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 
+/**
+ * Class ContentNegotiationMiddleware
+ *
+ * Middleware that verifies the 'Accept' header of incoming requests.
+ *
+ * @package App\Middleware
+ */
 class ContentNegotiationMiddleware implements MiddlewareInterface
 {
+    /**
+     * @var AppSettings Application settings instance.
+     */
     public function __construct(private AppSettings $app_settings) {}
 
+    /**
+     * Processes the request and ensures it accepts 'application/json'.
+     *
+     * @param Request $request Incoming server request.
+     * @param RequestHandler $handler Request handler to continue the pipeline.
+     *
+     * @throws HttpNotAcceptableException If the 'Accept' header is missing or invalid.
+     *
+     * @return ResponseInterface The processed response if the content type is acceptable.
+     */
     public function process(Request $request, RequestHandler $handler): ResponseInterface
     {
         $response = $handler->handle($request);
 
+        /**
+         * Extracts the 'Accept' header from the incoming request.
+         */
         $acceptHeader = $request->getHeaderLine('Accept');
 
+        /**
+         * Ensures the 'Accept' header includes 'application/json'.
+         */
         if (str_contains($acceptHeader, 'application/json')) {
             return $response;
         }
+
+        /**
+         * Throws an exception if the 'Accept' header does not include 'application/json'.
+         */
         if (!str_contains($acceptHeader, 'application/json')) {
             throw new HttpNotAcceptableException($request, "Not Acceptable");
         }
+
         return $response;
     }
 }
