ee6ded4433dd1487c1adbd7c9b9550de4053d36c
%apalex% <67773092+apalex@users.noreply.github.com>
2025-03-29 13:09:28 -0400

    forgot doc woops

--- app/src/Services/TeamsService.php
+++ app/src/Services/TeamsService.php
@@ -5,10 +5,28 @@ namespace App\Services;
 use App\Core\Result;
 use App\Models\TeamsModel;
 
+/**
+ * Class TeamsService
+ *
+ * Handles requests related to team data, including inserting team info
+ */
 class TeamsService
 {
+
+    /**
+     * TeamsService constructor
+     *
+     * @param TeamsModel $teamsModel The teams model instance.
+     */
     public function __construct(private TeamsModel $teamsModel) {}
 
+    /**
+     * Handles inserting a list of teams into database.
+     *
+     * @param array $newTeamsData The incoming data to insert.
+     * @param string $message The message to be sent after inserting inside the database.
+     *
+     */
     function createTeams(array $newTeamsData, string $message = "Team was successfully inserted into database!") : Result
     {
         //? Validate Received Data
