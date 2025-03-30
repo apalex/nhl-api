997b8e5c0b0ddd89bdfd37c15c3e52e7fd5105bb
2179710 <wayned27@outlook.com>
2025-03-30 14:05:15 -0400

    Update on root resource put Http methods available for each resources.

--- app/src/Controllers/AboutController.php
+++ app/src/Controllers/AboutController.php
@@ -4,16 +4,26 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Core\AppSettings;
 use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 
+/**
+ * Controller for handling the API root resource and metadata information.
+ *
+ * Provides a description of the web service, authorship, and available endpoints
+ * including HTTP method support, filters, and sorting capabilities.
+ */
 class AboutController extends BaseController
 {
     private const API_NAME = 'NHL-API';
     private const API_VERSION = '1.0.0';
 
-    public function handleAboutWebService(Request $request, Response $response): Response
+    /**
+     * Handles the request to retrieve metadata about the NHL API.
+     *
+     * @param Response $response The HTTP response to be returned.
+     * @return Response The JSON response containing metadata.
+     */
+    public function handleAboutWebService(Response $response): Response
     {
         $data = array(
             'api' => self::API_NAME,
@@ -26,63 +36,72 @@ class AboutController extends BaseController
                     'description' => 'Gets the list of teams',
                     'type' => 'collection resources',
                     'filters' => 'Team Name, Founding Year',
-                    'sorting' => 'Team Name, Founding Year'
+                    'sorting' => 'Team Name, Founding Year',
+                    'methods' => ['GET', 'POST']
                 ],
                 [
                     'uri' => '/teams/{team_id}',
                     'description' => 'Gets the details of the specified team',
                     'type' => 'singleton resources',
                     'filters' => 'N/A',
-                    'sorting' => 'N/A'
+                    'sorting' => 'N/A',
+                    'methods' => ['GET']
                 ],
                 [
                     'uri' => '/teams/{team_id}/games',
                     'description' => 'Gets the list of the specified team’s games',
                     'type' => 'sub-collection resources',
                     'filters' => 'Date (condition: greater than), Arena',
-                    'sorting' => 'Date, Arena'
+                    'sorting' => 'Date, Arena',
+                    'methods' => ['GET']
                 ],
                 [
                     'uri' => '/games',
                     'description' => 'Gets a list of games',
                     'type' => 'collection resources',
                     'filters' => 'Date (condition: greater than), Tournament Type',
-                    'sorting' => 'Date, Tournament Type'
+                    'sorting' => 'Date, Tournament Type',
+                    'methods' => ['GET']
                 ],
                 [
                     'uri' => '/games/{game_id}',
                     'description' => 'Gets the details of the specified game',
                     'type' => 'singleton resources',
                     'filters' => 'N/A',
-                    'sorting' => 'N/A'
+                    'sorting' => 'N/A',
+                    'methods' => ['GET', 'DELETE']
                 ],
                 [
                     'uri' => '/games/{game_id}/player_stats',
                     'description' => 'Gets the list of player statistics for a specific game',
                     'type' => 'sub-collection resources',
                     'filters' => 'First Name, Goals Scored, Assists, SOG',
-                    'sorting' => 'First Name, Goals Scored, Assists, SOG'
+                    'sorting' => 'First Name, Goals Scored, Assists, SOG',
+                    'methods' => ['GET']
                 ],
                 [
                     'uri' => '/arenas',
                     'description' => 'Gets the list of arenas where NHL games took place',
                     'type' => 'collection resources',
                     'filters' => 'Arena Name, Year Built (condition: greater than), Capacity (greater or lower), City, State/Province',
-                    'sorting' => 'Arena Name, Year Built, Capacity, City'
+                    'sorting' => 'Arena Name, Year Built, Capacity, City',
+                    'methods' => ['GET']
                 ],
                 [
                     'uri' => '/arenas/{arena_id}',
                     'description' => 'Gets the details of the specified arena',
                     'type' => 'singleton resources',
                     'filters' => 'N/A',
-                    'sorting' => 'N/A'
+                    'sorting' => 'N/A',
+                    'methods' => ['GET', 'PUT']
                 ],
                 [
                     'uri' => '/arenas/{arena_id}/games',
                     'description' => 'Gets the list of games played in the specified arena',
                     'type' => 'sub-collection resources',
                     'filters' => 'Date (condition: greater than), Tournament Type',
-                    'sorting' => 'Date, Tournament Type'
+                    'sorting' => 'Date, Tournament Type',
+                    'methods' => ['GET']
                 ]
             ]
         );
